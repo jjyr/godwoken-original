3be0a3d57c53953e4caa06370b29b2be9f4a0260
jjy <jjyruby@gmail.com>
2020-01-21 16:23:41 +0800

    complete deposit verification

--- contracts/main/src/action/deposit.rs
+++ contracts/main/src/action/deposit.rs
@@ -1,7 +1,6 @@
 use crate::{constants::Error, utils};
 use alloc::vec::Vec;
 use godwoken_types::{packed::*, prelude::*};
-use ckb_contract_std::debug;
 
 pub struct DepositVerifier<'a> {
     old_state: GlobalStateReader<'a>,
@@ -10,15 +9,6 @@ pub struct DepositVerifier<'a> {
 }
 
 impl<'a> DepositVerifier<'a> {
-    /// deposit capacity
-    fn deposit_capacity(&self) -> Result<u64, Error> {
-        let capacities = utils::fetch_capacities();
-        capacities
-            .output
-            .checked_sub(capacities.input)
-            .ok_or(Error::IncorrectCapacity)
-    }
-
     /// verify entry state
     fn verify_entry(&self, deposit_capacity: u64) -> Result<(), Error> {
         let old_entry = self.action.old_entry();
@@ -41,15 +31,10 @@ impl<'a> DepositVerifier<'a> {
         Ok(())
     }
 
-    fn verify_current_state(&self) -> Result<(), Error> {
-        use ckb_merkle_mountain_range::{leaf_index_to_pos, MerkleProof};
-        let old_entry_hash = {
-            let mut hash = [0u8; 32];
-            let mut hasher = utils::new_blake2b();
-            hasher.update(self.action.old_entry().as_slice());
-            hasher.finalize(&mut hash);
-            hash
-        };
+    fn verify_state_transition(&self) -> Result<(), Error> {
+        if self.old_state.block_root().as_slice() != self.new_state.block_root().as_slice() {
+            return Err(Error::InvalidGlobalState);
+        }
         let entries_count: u32 = self.action.count().unpack();
         let proof_items: Vec<[u8; 32]> = self
             .action
@@ -57,17 +42,24 @@ impl<'a> DepositVerifier<'a> {
             .iter()
             .map(|item| item.unpack())
             .collect();
+        // verify old_entry
+        let old_entry = self.action.old_entry();
         let old_account_root = self.old_state.account_root().unpack();
-        let calculated_account_root = utils::compute_account_root(old_entry_hash, entries_count - 1, entries_count, proof_items)?;
-        if calculated_account_root != old_account_root
-        {
-            return Err(Error::InvalidMerkleProof);
-        }
-        Ok(())
-    }
-
-    fn verify_state_transition(&self) -> Result<(), Error> {
-        // TODO
+        verify_entry_state(
+            old_entry,
+            &old_account_root,
+            entries_count,
+            proof_items.clone(),
+        )?;
+        // verify new_entry
+        let new_entry = self.action.new_entry();
+        let new_account_root = self.new_state.account_root().unpack();
+        verify_entry_state(
+            new_entry,
+            &new_account_root,
+            entries_count,
+            proof_items,
+        )?;
         Ok(())
     }
 }
@@ -86,10 +78,41 @@ impl<'a> DepositVerifier<'a> {
     }
 
     pub fn verify(&self) -> Result<(), Error> {
-        let deposit_capacity = self.deposit_capacity()?;
+        let deposit_capacity = deposit_capacity()?;
         self.verify_entry(deposit_capacity)?;
-        self.verify_current_state()?;
         self.verify_state_transition()?;
         Ok(())
     }
+}
+
+/// deposit capacity
+fn deposit_capacity() -> Result<u64, Error> {
+    let capacities = utils::fetch_capacities();
+    capacities
+        .output
+        .checked_sub(capacities.input)
+        .ok_or(Error::IncorrectCapacity)
+}
+
+/// verify entry state according to merkle root
+fn verify_entry_state<'a>(
+    entry: AccountEntryReader<'a>,
+    account_root: &[u8; 32],
+    entries_count: u32,
+    proof_items: Vec<[u8; 32]>,
+) -> Result<(), Error> {
+    let entry_hash = {
+        let mut hash = [0u8; 32];
+        let mut hasher = utils::new_blake2b();
+        hasher.update(entry.as_slice());
+        hasher.finalize(&mut hash);
+        hash
+    };
+    let entry_index = entry.index().unpack();
+    let calculated_account_root =
+        utils::compute_account_root(entry_hash, entry_index, entries_count, proof_items)?;
+    if &calculated_account_root != account_root {
+        return Err(Error::InvalidMerkleProof);
+    }
+    Ok(())
 }
